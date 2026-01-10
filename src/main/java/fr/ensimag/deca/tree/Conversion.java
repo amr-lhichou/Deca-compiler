29ca3104d07343d9780699cbe0d26497ac7092a5
aymen EL-HANAFY <aymen.el-hanafy@grenoble-inp.fr>
2026-01-10 23:45:29 +0100

    Etape C: Conversion : codegen

--- src/main/java/fr/ensimag/deca/tree/Conversion.java
+++ src/main/java/fr/ensimag/deca/tree/Conversion.java
@@ -8,10 +8,15 @@ import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.instructions.FLOAT;
+import fr.ensimag.ima.pseudocode.instructions.INT;
 
 public class Conversion extends AbstractExpr {
     AbstractExpr cible;
     AbstractIdentifier typeCible;
+    Type expType=null;
+    Type convType=null;
 
     public Conversion(AbstractIdentifier typeCible,AbstractExpr cible) {
         this.cible = cible;
@@ -22,7 +27,8 @@ public class Conversion extends AbstractExpr {
         
         Type expType = this.cible.verifyExpr(compiler, localEnv, currentClass);
         Type convType = this.typeCible.verifyType(compiler);
-
+        this.expType = expType;
+        this.convType = convType;
         if (!convType.isCastCompatible(expType)){
             throw new ContextualError("Le type " + expType + " et le type " + convType +
             " ne sont pas cast_compatible (règle 3.39)", getLocation());
@@ -30,6 +36,22 @@ public class Conversion extends AbstractExpr {
 
         setType(convType);
         return convType; 
+    }
+    @Override
+    protected void codeGenInst(DecacCompiler compiler){
+        cible.codeGenInst(compiler);
+        GPRegister R_target = compiler.getRegisterAllocater().getCurrentRegister();
+
+        Type typeSource = cible.getType(); // type de l'expression a droite
+        Type typeDest = this.getType();    // vers lequel on convertit
+
+        if (typeSource.isInt() && typeDest.isFloat()) {
+            compiler.addInstruction(new FLOAT(R_target, R_target));
+        }
+
+        else if (typeSource.isFloat() && typeDest.isInt()) {
+            compiler.addInstruction(new INT(R_target, R_target));
+        }
     }
 
     @Override
@@ -43,9 +65,18 @@ public class Conversion extends AbstractExpr {
 
     @Override
     protected void iterChildren(TreeFunction f) {
+//        typeCible.iter(f);
+//        cible.iter(f);
     }
 
     @Override
-    protected void prettyPrintChildren(PrintStream s, String prefix) {}
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+//        typeCible.prettyPrint(s, prefix, false);
+//        cible.prettyPrint(s, prefix, true);
+    }
+//    @Override
+//    String prettyPrintNode() {
+//        return "("+ expType +")" +" to " + "(" + convType + ")";
+//    }
 
 }
