9fa8263f4deedd8483610fe6c182074a77a25301
aymen EL-HANAFY <aymen.el-hanafy@grenoble-inp.fr>
2026-01-15 11:45:09 +0100

    partie C : objet- first pass

--- src/main/java/fr/ensimag/deca/tree/Program.java
+++ src/main/java/fr/ensimag/deca/tree/Program.java
@@ -43,6 +43,8 @@ public class Program extends AbstractProgram {
     @Override
     public void codeGenProgram(DecacCompiler compiler) {
         // A FAIRE: compléter ce squelette très rudimentaire de code
+        compiler.addComment("Code des tables des méthodes");
+        classes.codeGenListDeclClass(compiler);
         compiler.addComment("Main program");
         main.codeGenMain(compiler);
         compiler.addInstruction(new HALT());
