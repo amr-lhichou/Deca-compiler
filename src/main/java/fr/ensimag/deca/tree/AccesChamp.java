08ea7b6ba036c215d2dd9e3cb05eb4ab855fa401
Aymen EL-HANAFY <aymen.el-Hanafy@grenoble-inp.org>
2026-01-19 13:42:01 +0100

    etape C : debug

--- src/main/java/fr/ensimag/deca/tree/AccesChamp.java
+++ src/main/java/fr/ensimag/deca/tree/AccesChamp.java
@@ -108,9 +108,6 @@ public class AccesChamp extends AbstractLValue {
         this.identifiantChamp.iter(f);
     }
 
-    public AbstractIdentifier getIdentifiantChamp() {
-        return identifiantChamp;
-    }
 
 
 
