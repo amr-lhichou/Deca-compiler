b61697ec496e4706e8ae08705283564a190a3ee3
Aymen EL-HANAFY <aymen.el-hanafy@grenoble-inp.fr>
2026-01-19 12:44:39 +0100

    Partie C : Cast to object

--- src/main/java/fr/ensimag/deca/tree/Conversion.java
+++ src/main/java/fr/ensimag/deca/tree/Conversion.java
@@ -3,12 +3,13 @@ package fr.ensimag.deca.tree;
 import java.io.PrintStream;
 
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassDefinition;
-import fr.ensimag.deca.context.ContextualError;
-import fr.ensimag.deca.context.EnvironmentExp;
-import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.NullOperand;
+import fr.ensimag.ima.pseudocode.*;
+import fr.ensimag.ima.pseudocode.instructions.*;
 import fr.ensimag.ima.pseudocode.instructions.FLOAT;
 import fr.ensimag.ima.pseudocode.instructions.INT;
 
@@ -37,22 +38,7 @@ public class Conversion extends AbstractExpr {
         setType(convType);
         return convType; 
     }
-    @Override
-    protected void codeGenInst(DecacCompiler compiler){
-        cible.codeGenInst(compiler);
-        GPRegister R_target = compiler.getRegisterAllocater().getCurrentRegister();
 
-        Type typeSource = cible.getType(); // type de l'expression a droite
-        Type typeDest = this.getType();    // vers lequel on convertit
-
-        if (typeSource.isInt() && typeDest.isFloat()) {
-            compiler.addInstruction(new FLOAT(R_target, R_target));
-        }
-
-        else if (typeSource.isFloat() && typeDest.isInt()) {
-            compiler.addInstruction(new INT(R_target, R_target));
-        }
-    }
 
     @Override
     public void decompile(IndentPrintStream s) {
@@ -62,6 +48,50 @@ public class Conversion extends AbstractExpr {
         cible.decompile(s);
         s.print(")");
     }
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        cible.codeGenInst(compiler);
+        GPRegister R_target = compiler.getRegisterAllocater().getCurrentRegister();
+
+        Type srcType = cible.getType();
+        Type dstType = this.getType();
+
+        // int TO  float
+        if (srcType.isInt() && dstType.isFloat()) {
+            compiler.addInstruction(new FLOAT(R_target, R_target));
+            return;
+        }
+
+        // float TO int
+        if (srcType.isFloat() && dstType.isInt()) {
+            compiler.addInstruction(new INT(R_target, R_target));
+            return;
+        }
+
+        // cast dynamique classe/null -> classe
+        if (srcType.isClassOrNull() && dstType.isClass()) {
+            int id = compiler.getLabelId();
+            Label labelOk   = new Label("cast_ok_" + id);
+            Label labelLoop = new Label("cast_loop_" + id);
+            Label labelError = new Label("cast_error");
+
+            compiler.addInstruction(new CMP(new NullOperand(), R_target));
+            compiler.addInstruction(new BEQ(labelOk));
+            //we look for the vtable and we compare from parent to parent
+            int indexVTable = typeCible.getClassDefinition().getIndex_vtable();
+            compiler.addInstruction(new LEA(new RegisterOffset(indexVTable, Register.GB), Register.R1));
+            compiler.addInstruction(new LOAD(new RegisterOffset(0, R_target), Register.R0));
+            compiler.addLabel(labelLoop);
+            compiler.addInstruction(new CMP(Register.R1, Register.R0));
+            compiler.addInstruction(new BEQ(labelOk));
+            compiler.addInstruction(new CMP(new NullOperand(), Register.R0));
+            compiler.addInstruction(new BEQ(labelError));
+            compiler.addInstruction(new LOAD(new RegisterOffset(0, Register.R0), Register.R0));
+            compiler.addInstruction(new BRA(labelLoop));
+            compiler.addLabel(labelOk);
+        }
+    }
+
 
     @Override
     protected void iterChildren(TreeFunction f) {
